e10d8f14faa69c72b19b02332645e4d74e2c88cf
behdad <bmihanyar7@gmail.com>
2025-05-20 12:11:47 +0330

    FORM

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -45,7 +45,7 @@ const Profile = () => {
             Edit Info
           </p>
           <form
-            class="max-sm:w-85 mx-auto w-100 h-150 bg-gradient-to-b from-blue-400/70 to-blue-800/5 rounded-2xl flex justify-center flex-col"
+            className="max-sm:w-85 mx-auto w-100 h-150 bg-gradient-to-b from-blue-400/70 to-blue-800/5 rounded-2xl flex justify-center flex-col"
             onSubmit={(e) => submitHandler(e)}
           >
             <button
@@ -55,7 +55,7 @@ const Profile = () => {
               X
             </button>
             <div className="flex flex-col w-full h-100 justify-center gap-3 p-10">
-              <div class="">
+              <div className="">
                 <label
                   for="base-input"
                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -67,13 +67,13 @@ const Profile = () => {
                   type="text"
                   id="base-input"
                   onChange={(e) => dispatch(getName(e.target.value))}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
               <div class="">
                 <label
                   for="base-input"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Family Name
                 </label>
@@ -82,13 +82,13 @@ const Profile = () => {
                   type="text"
                   id="base-input"
                   onChange={(e) => dispatch(getFamily(e.target.value))}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
               <div>
                 <label
                   for="small-input"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Phone Number
                 </label>
@@ -97,13 +97,13 @@ const Profile = () => {
                   onChange={(e) => dispatch(getPhoneNumber(e.target.value))}
                   type="text"
                   id="large-input"
-                  class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
               <div class="mb-5">
                 <label
                   for="large-input"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Bio
                 </label>
@@ -113,7 +113,7 @@ const Profile = () => {
                   type="text"
                   onChange={(e) => dispatch(getbio(e.target.value))}
                   id="large-input"
-                  class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   rows="5"
                   cols="50"
                 >
@@ -130,29 +130,29 @@ const Profile = () => {
           </form>
         </div>
       ) : (
-        <div class="w-full h-screen px-10 pt-10">
-          <div class="relative mt-16 mb-10 max-w-sm mx-auto">
-            <div class="rounded overflow-hidden shadow-md bg-white h-150">
-              <div class="absolute -mt-20 w-full flex justify-center">
-                <div class="h-50 w-50">
+        <div className="w-full h-screen px-10 pt-10">
+          <div className="relative mt-16 mb-10 max-w-sm mx-auto">
+            <div className="rounded overflow-hidden shadow-md bg-white h-150">
+              <div className="absolute -mt-20 w-full flex justify-center">
+                <div className="h-50 w-50">
                   <img
                     src={select.image}
-                    class="rounded-full object-cover h-full w-full shadow-md"
+                    className="rounded-full object-cover h-full w-full shadow-md"
                   />
                 </div>
               </div>
-              <div class=" flex flex-col gap-5 justify-center px-6 mt-40">
-                <h1 class="font-bold text-3xl text-center mb-1">
+              <div className=" flex flex-col gap-5 justify-center px-6 mt-40">
+                <h1 className="font-bold text-3xl text-center mb-1">
                   {selectUser.name}
                 </h1>
-                <p class="text-gray-800 text-lg text-center">
+                <p className="text-gray-800 text-lg text-center">
                   {selectUser.phoneNumber}
                 </p>
-                <p class="text-gray-800 text-lg text-center">
+                <p className="text-gray-800 text-lg text-center">
                   {select.bio.length > 50 && select.bio.slice(0, 100)}
                 </p>
                 <button
-                  class="text-center text-gray-600 text-xl pt-3 font-bold cursor-pointer "
+                  className="text-center text-gray-600 text-xl pt-3 font-bold cursor-pointer "
                   onClick={() => bioHandler()}
                 >
                   Edit your info ...
